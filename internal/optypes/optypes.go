26737fe4d4740f1e6cea6470d9b697e16a8eb009
Jan Pfeifer <pfeifer@gmail.com>
2025-11-04 08:45:31 +0100

    Added missing multi-device operations. Change enumer generate file to prefix "gen_"

--- internal/optypes/optypes.go
+++ internal/optypes/optypes.go
@@ -10,7 +10,7 @@ import (
 // OpType is an enum of all generic operations that ToStableHLO can support -- not all are implemented yet.
 type OpType int
 
-//go:generate go tool enumer -type=OpType optypes.go
+//go:generate go tool enumer -type=OpType -output=gen_optype_enumer.go optypes.go
 
 const (
 	Invalid OpType = iota
@@ -20,6 +20,7 @@ const (
 
 	Abs
 	Add
+	AllReduce
 	And
 	Atan2
 	BatchNormInference
@@ -30,8 +31,7 @@ const (
 	Cbrt
 	Ceil
 	Clamp
-	CollectiveBroadcast // Added
-	CollectiveAllReduce // Added	Compare
+	CollectiveBroadcast
 	Compare
 	Complex
 	Concatenate
@@ -92,6 +92,8 @@ const (
 
 	// Here the ones not implemented yet, please add an issue in the repo if you need them.
 
+	AllGather
+	AllToAll
 	Case
 	Cholesky
 	CollectivePermute
@@ -128,9 +130,9 @@ var (
 	// stableHLOMappings maps OpType to the corresponding StableHLO name, when the default
 	// "snake case" doesn't work.
 	stableHLOMappings = map[OpType]string{
-		FuncReturn:          "stablehlo.return",
-		Erf:                 "chlo.erf",
-		CollectiveAllReduce: "stablehlo.all_reduce"}
+		FuncReturn: "stablehlo.return",
+		Erf:        "chlo.erf",
+		AllReduce:  "stablehlo.all_reduce"}
 )
 
 // ToStableHLO returns the ToStableHLO name of the operation.
